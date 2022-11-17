1202cf4596777a1c6686b1d4b133198b1e76b7cb
mushroom <mushroom019.coder@gmail.com>
2022-11-17 20:56:52 +0530

    homepage of the website

---
+++ src/components/Form.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Component } from 'react';
+import Name from './name';
+import Contact from './ContactInfo';
+import Education from './Education';
+import WorkExperience from './WorkExperience';
+import Skills from './skill';
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.skills = [];
+  }
+
+  clickHandle(e) {
+    e.preventDefault();
+  }
+  render() {
+    return (
+      <form>
+        <Name />
+        <Contact />
+        <fieldset>
+          <legend>Work Experience</legend>
+          <WorkExperience />
+        </fieldset>
+        <fieldset>
+          <legend>Education Information</legend>
+          <Education />
+        </fieldset>
+        <fieldset>
+          <legend>Skills</legend>
+          <Skills />
+        </fieldset>
+        <button typeof="submit">Generate Cv</button>
+      </form>
+    );
+  }
+}
+
+export default Form;
---
+++ src/components/main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Form from './Form';
+import Cv from './cv';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <Form />
+        <Cv />
+      </div>
+    );
+  }
+}
+
+export default Main;
